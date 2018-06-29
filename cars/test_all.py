7413a4dc1842910d73b39905463680771d8484bc
LAPTOP-2H2OKT8Q\lenovo <lana463684@163.com>
2018-06-29 17:23:21 +0800

    no message

---
+++ cars/test_all.py
@@ -0,0 +1,29 @@
+
+class Test_allure:
+    def setup(self):
+        pass
+    def teardown(self):
+        pass
+    def test_al(self):
+        assert 0
+
+
+
+
+
+# class TestABC:
+#     def setup_class(self):
+#         print("------->setup_class")
+#
+#     def teardown_class(self):
+#         print("------->teardown_class")
+#
+#     def test_a(self):
+#         print("------->test_a")
+#         assert 1
+#
+#     def test_b(self):
+#         print("------->test_b")
+#         assert 0  # 断言失败
+
+
